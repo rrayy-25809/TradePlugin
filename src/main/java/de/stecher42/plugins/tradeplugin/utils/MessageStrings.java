1e62d889830fcb143891349d6714627aa8a5a4d2
Robin St <robby3st@live.de>
2023-03-06 21:21:16 +0100

    Working on adding language support

--- src/main/java/de/stecher42/plugins/tradeplugin/utils/MessageStrings.java
+++ src/main/java/de/stecher42/plugins/tradeplugin/utils/MessageStrings.java
@@ -1,7 +1,39 @@
 package de.stecher42.plugins.tradeplugin.utils;
 
 import de.stecher42.plugins.tradeplugin.main.Main;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MessageStrings {
+    File languageFile;
+    FileConfiguration languageFileConfiguration;
     public static final String NO_PERMISSION = Main.PREFIX + "§cYou don't have the permissions to do that!";
+
+    public MessageStrings() {
+        this.createLanguageFile();
+    }
+
+    private void createLanguageFile() {
+        languageFile = new File(Main.getPlugin().getDataFolder() + "/languages/en_us.yml", "config.yml");
+        if (!languageFile.exists()) {
+            languageFile.getParentFile().mkdirs();
+            try {
+                languageFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Main.getPlugin().saveResource("en_us", false);
+        }
+//
+//        customConfig = new YamlConfiguration();
+//        try {
+//            customConfig.load(customConfigFile);
+//        } catch (IOException | InvalidConfigurationException e) {
+//            e.printStackTrace();
+//        }
+    }
 }
